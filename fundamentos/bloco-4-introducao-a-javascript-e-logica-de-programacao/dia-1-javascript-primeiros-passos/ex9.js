f994e4d97efd4b296ba7677d3724ba3e102ca3e0
Naillton <Nailton_junior@protonmail.com>
2021-11-27 11:34:19 -0600

    lucro de um produto

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex9.js
@@ -0,0 +1,15 @@
+const num1 = 100;
+const num2 = 200;
+
+let custo1 = 50;
+let custo2 = 120;
+
+let custoTotal1 = custo1 / 5;
+let custoTotal2 = custo2 / 5;
+let lucro1 = num1 - custoTotal1;
+let lucro2 = num2 - custoTotal2;
+
+let total1 = lucro1 * 1000;
+let total2 = lucro2 * 1000;
+console.log("A empresa vai ter como lucro do prouto 1 vendendo 1000 unidades = "+total1);
+console.log("\nA empresa vau ter como lucro do produto 2 vendendo 1000 unidades = "+total2);
