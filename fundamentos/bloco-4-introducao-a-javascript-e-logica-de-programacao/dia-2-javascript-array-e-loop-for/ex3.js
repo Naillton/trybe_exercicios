d1d12fe5e3fdb01730a05e4fa77d442399a881cf
Naillton <Nailton_junior@protonmail.com>
2021-11-29 16:21:21 -0600

    media aritmtica

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex3.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let i = 0; i < numbers.length; i++){
+   
+    soma = (soma + numbers[i]) / numbers.length;
+    
+}
+
+console.log(soma.toFixed());
