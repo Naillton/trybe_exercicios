543854379d82ce7d1b91c9ab804a6dd1cf72d0e3
Naillton <Nailton_junior@protonmail.com>
2021-11-30 10:15:10 -0600

    multiplicando valores de um array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
@@ -11,15 +11,30 @@
 //     }
 //   }
 
+// let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let index = 1; index <= array.length; index += 1) {
+//         console.log("\n"+"["+index+"]")
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//         console.log(array[secondIndex])
+//       if (array[index] > array[secondIndex]) {
+//         let position = array[index];
+//         array[index] = array[secondIndex];
+//         array[secondIndex] = position;
+//       }
+//     }
+//   }
+
+let numbers = [];
+
 let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let index = 1; index <= array.length; index += 1) {
-        console.log("\n"+"["+index+"]")
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-        console.log(array[secondIndex])
-      if (array[index] > array[secondIndex]) {
-        let position = array[index];
-        array[index] = array[secondIndex];
-        array[secondIndex] = position;
-      }
+
+for(let i = 0; i < array.length; i++){
+
+    if(i === array.length -1){
+        numbers.push(array[i]*2);
+    }else{
+        numbers.push(array[i]*array[i+1]);
     }
-  }
+}
+
+console.log(numbers);
