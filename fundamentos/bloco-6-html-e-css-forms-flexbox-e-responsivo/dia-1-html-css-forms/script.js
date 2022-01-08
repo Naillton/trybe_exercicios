34423e4292b7cee49b13a9d75024659f9f2a2905
Naillton <Nailton_junior@protonmail.com>
2022-01-08 06:24:34 -0600

    mudancas

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,3 +1,13 @@
+let text = document.querySelector("#texto");
+let mail = document.querySelector("#email");
+let cpf = document.querySelector("#cpf");
+let endereco = document.querySelector("#endereco");
+let estado = document.querySelector("#state");
+let resumo = document.querySelector("#resume");
+let cargo = document.querySelector("#cargo");
+let descricao = document.querySelector("#desc");
+let inicio = document.querySelector("#inicio");
+
 function adcEstado(){
     const selecao = document.querySelector("#state");
 
@@ -40,8 +50,29 @@ function adcEstado(){
 adcEstado();
 
 const btnEnv = document.querySelector("#btn-enviar")
-const inofrmacoes = document.querySelector("#infos")
+const btnLimp = document.querySelector("#btn-limp")
+const informacoes = document.querySelector("#infos")
 
 btnEnv.addEventListener("click", function(event){
     event.preventDefault();
+    let escolha = document.getElementsByName("escolha")
+    for(let i = 0; i < escolha.length; i ++){
+        if(escolha[i].checked){
+            informacoes.innerHTML = `${ text.value }    
+            ${ mail.value }     
+            ${ cpf.value }      
+            ${ endereco.value }     
+            ${ estado.value }
+            ${ escolha[i].value }              
+            ${ resumo.value }       
+            ${ cargo.value }        
+            ${ descricao.value }        
+            ${ inicio.value }`
+        }
+    }
 });
+
+btnLimp.addEventListener("click", function(){
+    informacoes.parentNode.removeChild(informacoes)
+});
+
