51af1a037b476e2310eb85baeb88bf051db49933
Naillton <Nailton_junior@protonmail.com>
2022-01-13 11:58:47 -0600

    ex6 completo

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ex6.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/ex6.js
@@ -14,4 +14,13 @@ const pal = (palavra) =>{
     return string
 }
 
-console.log(pal('Trybe x Aqui!'))
+let result = pal('Trybe x Aqui!')
+console.log(result)
+let array = ["Git", "Github", "HTML", "CSS", "JS", "FlexBox", "Mobile-first", "Soft-Skills", "ES6"]
+const retornar = (nome, frase) => {
+    let order = frase.sort()
+    return `${nome}
+Aprendizados ${order} #goTrybe`;
+}
+
+console.log(retornar(result, array));
