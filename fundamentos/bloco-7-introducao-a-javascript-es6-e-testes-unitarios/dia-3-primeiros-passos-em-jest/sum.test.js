9248cedf4cd82a5be457b7a6f912b310abea000b
Naillton <Nailton_junior@protonmail.com>
2022-01-14 21:03:51 -0600

    requisito 2 ok

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -4,4 +4,8 @@ describe('sum', () => {
     it('testa se o retorno de 4 e 5 e 9', () => {
         expect(sum(4, 5)).toBe(9);
     });
+
+    it('testa se o retorno de 0 e 0 e 0', () => {
+        expect(sum(0, 0)).toBe(0);
+    });
 });
