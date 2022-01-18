571ef9ade53865469fde9f373ab7f3fd93280534
Naillton <Nailton_junior@protonmail.com>
2022-01-18 15:15:14 -0600

    ex1.js

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/ex1.js
@@ -0,0 +1,18 @@
+const funcObj = (nome) => {
+    const obj = {
+        nomeCompleto: `${nome}`,
+        email: `${nome}@trybe.com`,
+    }
+    return obj;
+};
+
+const newEmployees = (funcObj) => {
+    const employees = {
+      id1: funcObj('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: funcObj('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: funcObj('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+
+  console.log(newEmployees(funcObj));
