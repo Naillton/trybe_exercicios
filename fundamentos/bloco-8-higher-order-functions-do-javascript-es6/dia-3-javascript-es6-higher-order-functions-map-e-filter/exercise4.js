df3d8c35fe753096d119d1aed2bd9e0c5e3fd544
Naillton <Nailton_junior@protonmail.com>
2022-01-20 19:42:48 -0600

    ordenacao de livros

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise4.js
@@ -0,0 +1,59 @@
+/* const expectedResult = [
+    {
+      id: 6,
+      name: 'O Chamado de Cthulhu',
+      genre: 'Terror',
+      author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+      releaseYear: 1928,
+    },
+    {
+      id: 3,
+      name: 'Fundação',
+      genre: 'Ficção Científica',
+      author: { name: 'Isaac Asimov', birthYear: 1920 },
+      releaseYear: 1951,
+    },
+    {
+      id: 2,
+      name: 'O Senhor dos Anéis',
+      genre: 'Fantasia',
+      author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+      releaseYear: 1954,
+    },
+  ]; */
+
+  const arrayObj = [
+    {
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+        releaseYear: 1954,
+    },
+    {
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+        releaseYear: 1928,
+    },
+
+    {
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: { name: 'Isaac Asimov', birthYear: 1920 },
+        releaseYear: 1951,
+    },
+  ];
+
+  const ano = new Date();
+  const year = ano.getFullYear();
+
+  function oldBooksOrdered(books) {
+    const filtred = books.filter((book) => book);
+    const ordened = filtred.sort((a, b) => a.releaseYear - b.releaseYear);
+    return ordened;
+  }
+
+  console.log(oldBooksOrdered(arrayObj));
