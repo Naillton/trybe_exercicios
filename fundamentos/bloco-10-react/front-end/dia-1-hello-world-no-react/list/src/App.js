7da9995f3725565138df76521ca80993355aa9a8
Naillton <Nailton_junior@protonmail.com>
2022-02-16 21:58:41 +0000

    exercicio 1

--- fundamentos/bloco-10-react/front-end/dia-1-hello-world-no-react/list/src/App.js
+++ fundamentos/bloco-10-react/front-end/dia-1-hello-world-no-react/list/src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 
-const Task = (value) => {
-  return (
-    <div>
-      <h1>Minha lista de tarefas aprendidas ate aqui</h1>
-      <ol className='ordened'>
-        <li>{value}</li>
-      </ol>
-    </div>
-  );
-}
-
 const listen = ['HTML', 'CSS', 'JavaScript', 'Unit Test', 'ES6', 'React'];
-const str = listen.join(' ');
+
+const Task = (value) => {
+  return value.map((listens) => {
+    return (
+          <li>{listens}</li>
+    );
+  });
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {Task(str)}
+        <div>
+        <h1>Minha lista de tarefas aprendidas ate aqui</h1>
+          <ol className='ordened'>
+            {Task(listen)}
+          </ol>
+        </div> 
       </header>
     </div>
   );
