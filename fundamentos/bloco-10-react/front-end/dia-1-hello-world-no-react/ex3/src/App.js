8d2b9927ab42bfecc53b67a9393dd7eefbae7d05
Naillton <Nailton_junior@protonmail.com>
2022-02-17 02:53:08 +0000

    exercicios ok

---
+++ fundamentos/bloco-10-react/front-end/dia-1-hello-world-no-react/ex3/src/App.js
@@ -0,0 +1,16 @@
+import Header from './header';
+import Content from './content';
+import Footer from './footer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
